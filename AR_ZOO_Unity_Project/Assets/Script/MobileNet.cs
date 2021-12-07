511953a9e72c49b3160b0ffb05574cb12a1dc60c
hypknot74 <momorock74@gmail.com>
2021-12-07 13:47:30 +0900

    add

---
+++ AR_ZOO_Unity_Project/Assets/Script/MobileNet.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Unity.Barracuda;
+
+public class MobileNet
+{   
+    readonly IWorker worker;
+
+    private int inputShapeX = 150;
+    private int inputShapeY = 150;
+
+    public MobileNet(NNModel modelAsset)
+    {
+        var model = ModelLoader.Load(modelAsset);
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+        Debug.Log("Worker:CPU");
+        worker = WorkerFactory.CreateWorker(WorkerFactory.Type.CSharpBurst, model); // CPU
+#else
+        Debug.Log("Worker:GPU");
+        worker = WorkerFactory.CreateWorker(WorkerFactory.Type.ComputePrecompiled, model); // GPU
+#endif
+    }
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+    public float[] Inference(Texture2D texture)
+    {
+        // テクスチャコピー
+        Texture2D inputTexture = new Texture2D(texture.width, texture.height);
+        var tempColor32 = texture.GetPixels32();
+        inputTexture.SetPixels32(tempColor32);
+        inputTexture.Apply();
+        Graphics.CopyTexture(texture, inputTexture);
+
+        // テクスチャリサイズ、およびColor32データ取得
+        TextureScale.Bilinear(inputTexture, inputShapeX, inputShapeY);
+        var color32 = inputTexture.GetPixels32();
+        MonoBehaviour.Destroy(inputTexture);
+        
+        float[] floatValues = new float[inputShapeX * inputShapeY * 3];
+        for (int i = 0; i < color32.Length; ++i) {
+            var color = color32[i];
+            floatValues[i * 3 + 0] = (color.r - 0) / 255.0f;
+            floatValues[i * 3 + 1] = (color.g - 0) / 255.0f;
+            floatValues[i * 3 + 2] = (color.b - 0) / 255.0f;
+        }
+
+        var inputTensor = new Tensor(1, inputShapeY, inputShapeX, 3, floatValues);
+
+        worker.Execute(inputTensor);
+        var outputTensor = worker.PeekOutput();
+        var outputArray = outputTensor.ToReadOnlyArray();
+        
+        inputTensor.Dispose();
+        outputTensor.Dispose();
+
+        return outputArray;
+    }
+#else
+    public float[] Inference(Texture2D texture)
+    {   
+        // ---Modified---
+        // テクスチャコピー
+        Texture2D inputTexture = new Texture2D(texture.width, texture.height);
+        var tempColor32 = texture.GetPixels32();
+        inputTexture.SetPixels32(tempColor32);
+        inputTexture.Apply();
+        Graphics.CopyTexture(texture, inputTexture);
+
+        // テクスチャリサイズ、およびColor32データ取得
+        TextureScale.Bilinear(inputTexture, inputShapeX, inputShapeY);
+        var color32 = inputTexture.GetPixels32();
+        MonoBehaviour.Destroy(inputTexture);
+        
+        float[] floatValues = new float[inputShapeX * inputShapeY * 3];
+        for (int i = 0; i < color32.Length; ++i) {
+            var color = color32[i];
+            floatValues[i * 3 + 0] = (color.r - 0);
+            floatValues[i * 3 + 1] = (color.g - 0);
+            floatValues[i * 3 + 2] = (color.b - 0);
+        }
+
+        var inputTensor = new Tensor(1, inputShapeY, inputShapeX, 3, floatValues);
+
+        // var inputTensor = new Tensor(texture, 3);
+        // --------------
+
+        worker.Execute(inputTensor);
+        var outputTensor = worker.PeekOutput();
+        var outputArray = outputTensor.ToReadOnlyArray();
+        
+        inputTensor.Dispose();
+        outputTensor.Dispose();
+
+        return outputArray;
+    }
+#endif
+
+    ~MobileNet()
+    {
+        worker?.Dispose();
+    }
+
+    public string getClassName(int classId)
+    {
+        if (classId < 0 || className.Length <= classId){
+            return "";
+        }
+        return className[classId];
+    }
+
+    string[] className = {
+        "0_Buildings",
+        "1_Forests",
+        "2_Glacier",
+        "3_Mountains",
+        "4_Sea",
+        "5_Street"
+    };
+
+}
---
+++ AR_ZOO_Unity_Project/Assets/Script/WebCamController.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Unity.Barracuda;
+
+public class WebCamController : MonoBehaviour
+ {
+    int width = 640;
+    int height = 480;
+    int fps = 30;
+    WebCamTexture webcamTexture;
+
+    // MobileNetモデル関連
+    public NNModel modelAsset;    
+    private MobileNet mobileNet;
+    
+    Texture2D texture;
+    Color32[] color32;
+
+    // GameObject を定義
+    private GameObject[] ARObjects;
+
+    // Startから5秒経っているかのFlag
+    private bool IsProcessingFlag;
+    private float elapsedTime;
+    private float[] scoreSums;
+    private int cnt;
+
+    // 推論結果描画用テキスト
+    public Text text;
+    private readonly FPSCounter fpsCounter = new FPSCounter();
+
+    void Start() 
+    {   
+
+        // ARObject の初期化
+        ARObjects = getARObjects();
+        InitializeARObjects();
+
+        // Webカメラ準備
+        WebCamDevice[] devices = WebCamTexture.devices;
+        webcamTexture = new WebCamTexture(devices[0].name, this.width, this.height, this.fps);
+        webcamTexture.Play();
+        
+        // MobileNetV2推論用クラス
+        mobileNet = new MobileNet(modelAsset);
+        StartCoroutine(InitializeWebCamTexture());
+    }
+
+    IEnumerator InitializeWebCamTexture()
+    {
+        while (true) {
+            Debug.Log(webcamTexture.width);
+            Debug.Log(webcamTexture.height);
+            if (webcamTexture.width > 16 && webcamTexture.height > 16) {
+                GetComponent<Renderer>().material.mainTexture = webcamTexture;
+                color32 = new Color32[webcamTexture.width * webcamTexture.height];
+                texture = new Texture2D(webcamTexture.width, webcamTexture.height);
+                break;
+            }
+            yield return null;
+        }
+    }
+
+    void InitializeARObjects(){
+        foreach (GameObject ARObject in ARObjects) {
+            ARObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {   
+        if (color32 != null && texture != null){
+
+            if (Input.GetKey(KeyCode.Space)){
+                elapsedTime = 0;
+                IsProcessingFlag = StartProcess();
+            }
+
+            if (IsProcessingFlag){
+                elapsedTime += Time.deltaTime;
+                Process();
+
+                if (elapsedTime > 5f){
+                    IsProcessingFlag = EndProcess();
+                }
+            }
+        }
+
+    }
+
+    private bool StartProcess(){
+        InitializeARObjects();
+        scoreSums = new float[ARObjects.Length];
+        cnt = 0;
+        return true;
+    }
+
+    private bool EndProcess(){
+        var maxScore = float.MinValue;
+        int classId = -1;
+        for (int i = 0; i < scoreSums.Length; i++) {
+            if (maxScore < scoreSums[i]) {
+                maxScore = scoreSums[i];
+                classId = i;
+            }
+        }
+        // 推論の最終結果テキスト構築とARObjectの表示
+        float maxScoreRate = maxScore/cnt;
+        string resultText = "";
+        resultText += "--- Result ---\n";
+        resultText += "Class ID: " + classId.ToString() + "\n";
+        resultText += "Score: " + maxScoreRate.ToString("F3") + "\n";
+
+        if (classId >= 0) {
+
+            ARObjects[classId].SetActive(true);
+            
+            resultText += "Name: " + mobileNet.getClassName(classId) + "\n";
+        } else {
+            resultText += "Name:????\n";
+        }
+
+        // テキスト画面反映
+        text.text = resultText;
+
+        return false;
+    }
+
+    private void Process(){
+        
+        // Inference 実行回数
+        cnt++;
+
+        // fps のアップデート
+        fpsCounter.Update();
+
+        // 入力用テクスチャ準備
+        webcamTexture.GetPixels32(color32);
+        texture.SetPixels32(color32);
+        texture.Apply();
+        
+        // 推論
+        var scores = mobileNet.Inference(texture);
+        
+        // 推論結果とテキスト構築
+        string resultText = "";
+        resultText += "FPS:" + fpsCounter.FPS.ToString("F2") + "\n" + "\n";  
+
+        // score各クラスのスコアを加算する
+        for (int i = 0; i < scores.Length; i++) {
+            scoreSums[i] += scores[i];
+            resultText += scores[i].ToString("F3") + " : " + mobileNet.getClassName(i) + "\n";
+        }
+        #if UNITY_IOS || UNITY_ANDROID
+                resultText += SystemInfo.graphicsDeviceType;
+        #endif
+
+        // テキスト画面反映
+        text.text = resultText;
+    }
+
+// ==========ARObjects==========
+
+    public GameObject Buildings;
+    public GameObject Forests;
+    public GameObject Glacier;
+    public GameObject Mountains;
+    public GameObject Sea;
+    public GameObject Street;
+
+    private GameObject[] getARObjects()
+    {
+        var ARObjects = new GameObject[]
+        {
+          Buildings,
+          Forests,
+          Glacier,
+          Mountains,
+          Sea,
+          Street
+        };
+        
+        return ARObjects;
+    }
+
+}
